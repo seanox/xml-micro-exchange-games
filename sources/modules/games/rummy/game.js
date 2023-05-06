aa6d7a9e219bcf026fc3e2e0482d531e83a35807
seanox <service@seanox.de>
2023-05-06 11:28:23 +0200

    #0000 Implementation

---
+++ sources/modules/games/rummy/game.js
@@ -0,0 +1,23 @@
+const VIEW_INTRODUCTION = "introduction";
+const VIEW_SETTINGS = "settings";
+const VIEW_INVITATION = "invitation";
+const VIEW_DESK = "desk";
+const VIEW_SCORE = "score";
+
+let game = {
+    get name() {
+        return GAME_RUMMY;
+    },
+    get disabled() {
+        return true;
+    },
+    init() {
+    },
+    dispose() {
+    }
+};
+
+game = Reactive(game);
+game = Observer(game);
+
+#export game@games.rummy;
