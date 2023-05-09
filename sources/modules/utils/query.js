bf285b4277afb951a84ddd02ab54e893e11a36d3
seanox <service@seanox.de>
2023-05-09 07:38:33 +0200

    #0000 Implementation

--- sources/modules/utils/query.js
+++ sources/modules/utils/query.js
@@ -2,44 +2,37 @@ const LOCATION = window.location.href
     .replace(/[?#].*$/, "")
     .replace(/\/+$/, "");
 
+const _create = (data) => {
+    if (!Array.isArray(data))
+        data = [];
+    if (data.length <= 0)
+        return "";
+    const meta = [];
+    data.forEach(data => {
+        if (!Array.isArray(data)) {
+            if (data === null
+                    || data === undefined)
+                data = "";
+            meta.push(encodeURIComponent(String(data)));
+        } else meta.push(_create(data).substring(1));
+    })
+    return "/" + meta.join("/");
+};
+
 const query = {
-    create(meta) {
-        const parameters = new URLSearchParams();
-        const collect = (name, value, manipulate) => {
-            value = String(value || "");
-            if (typeof manipulate === "function")
-                value = manipulate.call(value);
-            value = value.trim();
-            if (value)
-                parameters.append(name, value);
-        }
-        collect("game", meta.game, String.prototype.toLowerCase());
-        collect("options", meta.options);
-        Array.from(meta.players || [])
-            .filter(player =>
-                player.trim().length > 0)
-            .forEach((player) =>
-                collect("players", player));
-        const query = parameters.toString();
-        return LOCATION + (query ? "?" + query : "");
+    create(data) {
+        console.log(data);
+        const query = _create(data) || "";
+        return LOCATION + (query ? "#" + query : "");
     },
     read() {
-        const searchParams = (new URL(document.location)).searchParams;
-        if (!String(searchParams))
+        const hash = document.location.hash.substring(1);
+        if (!hash.startsWith("/"))
             return;
-        const meta = {};
-        if (searchParams.has("game"))
-            meta.game = searchParams.get("game");
-        if (searchParams.has("options"))
-            meta.options = searchParams.get("options");
-        if (searchParams.has("players"))
-            meta.players = searchParams.getAll("players");
-        if (searchParams.has("player"))
-            meta.player = searchParams.get("player");
-        return meta;
+        return hash.split("/").slice(1);
     },
-    update(meta) {
-        const url = this.create(meta);
+    update(data) {
+        const url = this.create(data);
         const object = {title: document.title, url:url};
         window.history.replaceState(object, object.title, object.url);
     },
