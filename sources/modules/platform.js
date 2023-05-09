9d7226af415978bcfa6f19d232a70019d3585c62
seanox <service@seanox.de>
2023-05-09 07:37:09 +0200

    #0000 Implementation

--- sources/modules/platform.js
+++ sources/modules/platform.js
@@ -1,3 +1,6 @@
+// import of runtime extensions
+#import utils/query;
+
 // I have not yet found a way to determine the modules dynamically.
 // The directory index would be a possibility, but that can be disabled and then
 // it doesn't work.
@@ -12,11 +15,18 @@ const PATTERN_PLAYER_ALIAS = "^[0-9a-zA-Z\\xC4\\xE4\\xD6\\xF6\\xDC\\xFC]{1,8}$";
 
 #export PATTERN_PLAYER_ALIAS;
 
+const GAMES = [GAME_MEMORY, GAME_RUMMY, GAME_SNAKE];
+
 const platform = Reactive({
+    dock() {
+        const meta = utils.query.read();
+        if (meta && meta.length > 0 && GAMES.includes(meta[0]))
+            platform.selection = meta[0];
+    },
     get games() {
         // resources of the games are preloaded
         // e.g. the texts are necessary for the use in the selection
-        const games = [GAME_MEMORY, GAME_RUMMY, GAME_SNAKE];
+        const games = Array.from(GAMES);
         games.forEach(game =>
             Composite.include(...["games", game, "game"]));
         return games.map(game => window["games"][game]["game"]);
